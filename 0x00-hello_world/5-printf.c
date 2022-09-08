4a9af211cb8079f917513745a18efe0f54ce12a7
mishatheking <anikemionak@gmail.com>
2022-09-08 11:21:42 -0700

    sizeof

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 * main - printing
 *
 *Return: Always 0 (Successful)
-*/   
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+return (0);
 }
