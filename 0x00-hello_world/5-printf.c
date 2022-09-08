aa1a0dde4919d8a1319890aad516fe13befcfea9
mishatheking <anikemionak@gmail.com>
2022-09-08 11:06:54 -0700

    Hello, print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,15 @@
-#!/bin/bash
-
 #include <stdio.h>
+/**
 
-int main(void){
-  puts("Programming is like building a multilingual puzzle.\n");
-  return 0;
-   }
+* main - printing
+
+*
+
+*Return: Always 0 (Successful)
+
+ */   
+ int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
+}
