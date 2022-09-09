4970788d8d8046ecb1176fe3239acac3b2b5f82e
mishatheking <anikemionak@gmail.com>
2022-09-09 09:12:14 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,13 +8,11 @@
 
 
 
-/**                                                                            
-
- * main - Positive or Negative - print sign of random number                    
-
- * @void: Empty parameter list for main.                                     
-
- * Return: 0 for success                                                      */
+/**                                                                        
+* main - Positive or Negative - print sign of random number                  
+* @void: Empty parameter list for main.                                     
+* Return: 0 for success                                                     
+*/
 
 int main(void) 
 {  
@@ -22,10 +20,10 @@ int n;
 srand(time(0));  
 n = rand() - RAND_MAX / 2;
 if ((n % 10) == 0)
-printf("Last digit of %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 else if ((n % 10) > 5)
-printf("Last digit of %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 else
-printf("Last digit of %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 return (0);
-}  
+}
