6e8594f7d28519e5c06c8cc8e44a7dea4f830720
mishatheking <anikemionak@gmail.com>
2022-09-30 06:27:41 -0700

    Create the static library libmy.a containing all the functions listed

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _atoi -  converts  s to int
+* @s: s -  Variable
+* Return: Always 0.
+*/
+
+int _atoi(char *s)
+{
+s += 1;
+return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen - Returns the length of a string.
+* @str: string.
+*
+* Return: length.
+*/
+
+int _strlen(char *str)
+{
+int len = 0;
+while (*str++)
+len++;
+return (len);
+}
